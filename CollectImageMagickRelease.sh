5985dddfa47a75951107dac5b2ca19685f409776
Kostiantyn Gominyuk <kgominyuk@readdle.com>
2020-07-27 16:51:22 +0200

    Update include folder structure generation

--- CollectImageMagickRelease.sh
+++ CollectImageMagickRelease.sh
@@ -8,10 +8,13 @@ cp -r VisualMagick/bin Release/ImageMagick/
 cp -r VisualMagick/lib Release/ImageMagick/
 
 mkdir Release/ImageMagick/include
-find ImageMagick/Magick++/lib -name \*.h -exec cp --parents {} Release/ImageMagick/include \;
-find ImageMagick/MagickCore -name \*.h -exec cp --parents {} Release/ImageMagick/include \;
-find ImageMagick/MagickWand -name \*.h -exec cp --parents {} Release/ImageMagick/include \;
-
+cd ImageMagick
+cd Magick++/lib
+find . -name \*.h -exec cp --parents {} ../../../Release/ImageMagick/include \;
+cd ../../
+find MagickCore -name \*.h -exec cp --parents {} ../Release/ImageMagick/include \;
+find MagickWand -name \*.h -exec cp --parents {} ../Release/ImageMagick/include \;
+cd ..
 mkdir Release/ImageMagick/pdb
 find VisualMagick/Release -name \*.pdb -exec cp --parents {} Release/ImageMagick/pdb \;
 
